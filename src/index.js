ff93589d6ec95a9d2922417b135d8a53a3a8878f
Eugene Aslanov <e.aslanov@hh.ru>
2022-04-28 18:08:04 +0300

    add useState, useMemo hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
 import './index.css';
+import { restaurants } from './fixtures';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />, document.getElementById('root'));
+root.render(
+    <App restaurants={restaurants} />,
+    document.getElementById('root')
+);
